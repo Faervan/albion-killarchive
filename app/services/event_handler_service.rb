b6dba85df67f5a2aabebbeeeb42bc32756916aa3
faervan <blackhunter77@gmx.de>
2024-09-04 22:00:36 +0200

    fix: unique mount name fetching not working

--- app/services/event_handler_service.rb
+++ app/services/event_handler_service.rb
@@ -4,7 +4,8 @@ class String
   def parse_item
     match = match(/(?:T(?<tier>[1-8])_)?(?<double>2H_)?(?<item_type>[^@]*)(?:@(?<enchantment>[0-4]))?/)
     {
-      path: "T#{match[:tier]}_#{match[:double] || ''}#{match[:item_type]}@#{match[:enchantment] || 0}",
+      path: (match[:tier].to_i.positive? ? "T#{match[:tier]}_" : '') +
+        "#{match[:double] || ''}#{match[:item_type]}@#{match[:enchantment] || 0}",
       tier: match[:tier].to_i,
       enchantment: match[:enchantment].to_i
     }
