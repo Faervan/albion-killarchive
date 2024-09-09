906252faaf05ab42a6bf4cadd67fb7ddc4169cae
faervan <blackhunter77@gmx.de>
2024-09-09 23:22:59 +0200

    dev sync: chaotic

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class GuildsController < ApplicationController
+  include SetParamsConcern
+  before_action -> { set_controller_params(params:, order: 'total_kill_count') }
+
   def index
     guild = Guild.includes([:alliance])
     if params[:alliance_id]
       guild = guild.where(alliance_id: params[:alliance_id])
       @alliance_id = params[:alliance_id]
     end
-    params[:list] = '20' unless params[:list]
-    params[:order_by] = 'total_kill_count' unless params[:order_by]
     @guilds = guild.order(params[:order_by]).reverse_order.limit(params[:list].to_i)
     @alliances = Alliance.all
-    @list = params[:list].to_i
+    @params = params
   end
 
   def show
-    params[:list] = '20' unless params[:list]
     @guild = Guild.find_by(name: params[:name])
     @players = Player
                .includes([:guild])
@@ -23,6 +23,6 @@ class GuildsController < ApplicationController
                .reverse_order
                .where(guild: @guild)
                .limit(params[:list].to_i)
-    @list = params[:list].to_i
+    @params = params
   end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 class PlayersController < ApplicationController
+  include SetParamsConcern
+  before_action -> { set_controller_params(params:, order: 'total_kill_count') }, only: [:index]
+  before_action -> { set_controller_params(params:, order: :timestamp) }, only: [:show]
+
   def index
     player = Player.includes([:guild])
     if params[:guild_id]
       player = player.where(guild_id: params[:guild_id])
       @guild_id = params[:guild_id]
     end
-    params[:list] = '20' unless params[:list]
-    params[:order_by] = 'total_kill_count' unless params[:order_by]
-    @players = player.order(params[:order_by]).reverse_order.limit(params[:list].to_i)
+    @players = player.order(params[:order_by]).reverse_order.limit(params[:list])
     @guilds = Guild.all
-    @list = params[:list].to_i
+    @params = set_controller_params(params:, order: 'total_kill_count')
   end
 
   def show
     @player = Player.find_by(name: params[:name])
-    params[:list] = '20' unless params[:list]
     kill_event = KillEvent.includes(%I[kill death]).order(:timestamp).reverse_order.limit(params[:list].to_i)
     @kill_events = kill_event
                    .where(kill: { player_id: @player.player_id })
                    .or(kill_event.where(death: { player_id: @player.player_id }))
-    @list = params[:list].to_i
   end
 end
