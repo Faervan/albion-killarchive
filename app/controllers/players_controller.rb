49eae6516bb475e29571df28d7e26ab522f45686
faervan <blackhunter77@gmx.de>
2024-09-10 23:28:55 +0200

    fix: rubocop

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -19,7 +19,7 @@ class PlayersController < ApplicationController
     params[:list] = '20' unless params[:list]
     params[:order_by] = 'timestamp' unless params[:order_by]
     @kill_events = KillEvent.includes(%I[kill death])
-      .where(kill: { player_id: @player.player_id })
+                            .where(kill: { player_id: @player.player_id })
                             .limit(params[:list].to_i)
   end
 end
